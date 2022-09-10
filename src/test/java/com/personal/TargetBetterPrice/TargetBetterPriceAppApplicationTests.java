a42201414bf8e69b1ccb0eef68ce7b42d784cb12
Luis-Medina12 <luis.medina0312@gmail.com>
2022-09-10 00:48:50 -0700

    Initial commit

---
+++ src/test/java/com/personal/TargetBetterPrice/TargetBetterPriceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.TargetBetterPrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargetBetterPriceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
