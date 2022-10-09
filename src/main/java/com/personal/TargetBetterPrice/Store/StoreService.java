a4559c9d34ca0137d4dcbdd4a145505827b4afad
Luis-Medina12 <luis.medina0312@gmail.com>
2022-10-09 01:31:54 -0700

    Cleaned up code, and added some exception handling

--- src/main/java/com/personal/TargetBetterPrice/Store/StoreService.java
+++ src/main/java/com/personal/TargetBetterPrice/Store/StoreService.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -18,76 +19,117 @@ import java.util.ArrayList;
 public class StoreService implements StoreInterface{
 
     private final ArrayList<Store> stores = new ArrayList<>();
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final HttpHeaders headers = new HttpHeaders();
+    private final ApiKeys apiKey = new ApiKeys();
 
     @Override
     public void insertStores(String homeZip, int tcin, String distance) {
 
-        ApiKeys apiKey = new ApiKeys();
-        RestTemplate restTemplate = new RestTemplate();
+        String storeURI = "https://230fa55e-acf6-4912-8733-05b3164c1d59.mock.pstmn.io/multiStore";
+//                "https://target-com-store-product-reviews-locations-data.p.rapidapi.com/location/search?zip="
+//                +homeZip+"&radius="+ distance;
 
-        String storeURI = "https://target-com-store-product-reviews-locations-data.p.rapidapi.com/location/search?zip="
-                +homeZip+"&radius="+ distance;
-
-        //"https://4ba751f1-9947-47b0-b376-3fd9d1632b98.mock.pstmn.io";
-
-        HttpHeaders headers = new HttpHeaders();
         headers.set("X-RapidAPI-Key", apiKey.getRapidAPI());
         headers.set("X-RapidAPI-Host","target-com-store-product-reviews-locations-data.p.rapidapi.com");
 
         HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
         ResponseEntity<String> response = restTemplate.exchange(storeURI, HttpMethod.GET,
                 requestEntity,String.class);
+        JsonObject data = JsonParser.parseString(response.getBody()).getAsJsonObject();
 
-        JsonObject data = new JsonParser().parse(response.getBody()).getAsJsonObject();
+        // extract locations from response
         JsonArray jsonArray = (JsonArray) data.get("locations");
-        JsonObject storeLoc = (JsonObject) jsonArray.get(0);
-        JsonObject storeAddress = (JsonObject) storeLoc.get("address");
 
-        // Store ID
-        String storeId = storeLoc.get("location_id").toString();
+        // Goes through every location returned within radius, and adds it to list
+        // Extracts storeID, storeZIP, and distance
+        for(int i = 0; i < jsonArray.size(); i++)
+        {
+            JsonObject storeLoc = (JsonObject) jsonArray.get(i);
+            JsonObject storeAddress = (JsonObject) storeLoc.get("address");
+            // Store ID
+            String storeId = storeLoc.get("location_id").toString();
 
-        // Store Distance from home ZIP
-        String storeDist = storeLoc.get("distance").toString();
+            // Store Distance from home ZIP
+            String storeDist = storeLoc.get("distance").toString();
 
-        // ZIP CODE
-        String zip = storeAddress.get("postal_code").toString().substring(1,6);
+            // Store's Zip Code
+            String zip = storeAddress.get("postal_code").toString().substring(1,6);
 
-        Store store = new Store(storeId,storeDist,zip);
+            double productPrice = getProductPrice(storeId,tcin);
+            Store store = new Store(storeId,storeDist,zip);
 
-        String productURI = "https://target-com-store-product-reviews-locations-data.p.rapidapi.com/product/details?store_id=" +
-                storeId+"&tcin=" + tcin;
-        headers.set("X-RapidAPI-Key", apiKey.getRapidAPI());
-        headers.set("X-RapidAPI-Host", "target-com-store-product-reviews-locations-data.p.rapidapi.com");
+            // get tax rate and set it to new store Object
+            store.setTaxRate(getTaxRate(zip));
 
-        HttpEntity<Void> productRequest = new HttpEntity<>(headers);
-        ResponseEntity<String> productResponse = restTemplate.exchange(productURI, HttpMethod.GET,
-                productRequest,String.class);
+            // get tax rate and set it to new store Object
+            store.setCurrentPrice(productPrice);
 
-        JsonObject productData = new JsonParser().parse(productResponse.getBody()).getAsJsonObject();
-        JsonObject productInfo = (JsonObject) productData.get("product");
-        JsonObject priceInfo = (JsonObject) productInfo.get("price");
+            // insert created store into the array
+            stores.add(store);
+        }
 
-        // Price of requested product
-        double productPrice = Double.parseDouble(priceInfo.get("current_retail").toString());
-
-        String taxURI = "https://api.apilayer.com/tax_data/tax_rates?zip=" + zip + "&country=US";
-        headers.set("apikey", "48n1QGuhg6DP7ERxniyNs0l1L0ItasrH");
-
-        HttpEntity<Void> taxRequest = new HttpEntity<>(headers);
-        ResponseEntity<String> taxResponse = restTemplate.exchange(taxURI, HttpMethod.GET,
-                taxRequest, String.class);
-
-        JsonObject taxData = new JsonParser().parse(taxResponse.getBody()).getAsJsonObject();
-        store.setTaxRate(Double.parseDouble(taxData.get("combined_rate").toString()));
-        store.setCurrentPrice(productPrice);
-
-        stores.add(store);
+        // Loop will remove any store where product isn't available (price = 0)
+        for(int i=0; i< stores.size(); i++)
+        {
+            if(stores.get(i).getCurrentPrice() == 0.0)
+            {
+                stores.remove(i);
+            }
+        }
 
     }
 
     @Override
     public ArrayList<Store> getStores() {
         return stores;
+    }
+
+    // Returns effective tax rate for zip code
+    public double getTaxRate(String storeZip)
+    {
+        String taxURI = "https://api.apilayer.com/tax_data/tax_rates?zip=" + storeZip + "&country=US";
+        headers.set("apikey", apiKey.getApiLayer());
+
+        HttpEntity<Void> taxRequest = new HttpEntity<>(headers);
+        ResponseEntity<String> taxResponse = restTemplate.exchange(taxURI, HttpMethod.GET,
+                taxRequest, String.class);
+
+        JsonObject taxData = JsonParser.parseString(taxResponse.getBody()).getAsJsonObject();
+        return Double.parseDouble(taxData.get("combined_rate").toString());
+    }
+
+    // Returns product price if available at store
+    // If call returns 404 then not in stock, so returns 0;
+    public double getProductPrice(String storeID, int TCIN)
+    {
+
+        String productURI = "https://230fa55e-acf6-4912-8733-05b3164c1d59.mock.pstmn.io/productDetails";
+
+//                "https://target-com-store-product-reviews-locations-data.p.rapidapi.com/product/details?store_id=" +
+//                storeId+"&tcin=" + tcin;
+        headers.set("X-RapidAPI-Key", apiKey.getRapidAPI());
+        headers.set("X-RapidAPI-Host", "target-com-store-product-reviews-locations-data.p.rapidapi.com");
+
+        HttpEntity<Void> productRequest = new HttpEntity<>(headers);
+
+        try{
+            ResponseEntity<String> productResponse = restTemplate.exchange(productURI, HttpMethod.GET,
+                    productRequest,String.class);
+
+            JsonObject productData = JsonParser.parseString(productResponse.getBody()).getAsJsonObject();
+            JsonObject productInfo = (JsonObject) productData.get("product");
+            JsonObject priceInfo = (JsonObject) productInfo.get("price");
+
+            return Double.parseDouble(priceInfo.get("current_retail").toString());
+        }catch (HttpStatusCodeException e)
+        {
+            if( e.getRawStatusCode() == 404)
+            {
+                return 0;
+            }
+        }
+        return 0;
     }
 
 }
--- src/main/java/com/personal/TargetBetterPrice/TargetBetterPriceAppApplication.java
+++ src/main/java/com/personal/TargetBetterPrice/TargetBetterPriceAppApplication.java
@@ -26,49 +26,7 @@ public class TargetBetterPriceAppApplication {
 	@Bean
 	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
 		return args -> {
-//			int zipCode = 85017;//aquireZIP();
-//
-//			String storeURI = "https://api.apilayer.com/tax_data/tax_rates?zip=85017&country=US";
 
-//			HttpHeaders headers = new HttpHeaders();
-//			headers.set("apikey", "48n1QGuhg6DP7ERxniyNs0l1L0ItasrH");
-//
-//			HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
-//			ResponseEntity<String> response = restTemplate.exchange(storeURI, HttpMethod.GET,
-//					requestEntity,String.class);
-//
-//			JsonObject data = new JsonParser().parse(response.getBody()).getAsJsonObject();
-//
-//			double taxRate = Double.parseDouble(data.get("combined_rate").toString());
-//			System.out.println(taxRate);
-//			String storeURI = "https://4ba751f1-9947-47b0-b376-3fd9d1632b98.mock.pstmn.io";
-//			// Actual Api works, setting to test Server for now to save server calls
-//			//"https://target-com-store-product-reviews-locations-data.p.rapidapi.com/location/search?zip="+zipCode+"&radius=3";
-//			HttpHeaders headers = new HttpHeaders();
-//			headers.set("X-RapidAPI-Key", "Key");
-//			headers.set("X-RapidAPI-Host","target-com-store-product-reviews-locations-data.p.rapidapi.com");
-//
-//			HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
-//			ResponseEntity<String> response = restTemplate.exchange(storeURI, HttpMethod.GET,
-//					requestEntity,String.class);
-//
-//			JsonObject data = new JsonParser().parse(response.getBody()).getAsJsonObject();
-//			JsonArray jsonArray = (JsonArray) data.get("locations");
-//
-//			JsonObject storeLoc = (JsonObject) jsonArray.get(0);
-//            JsonObject storeAddress = (JsonObject) storeLoc.get("address");
-//			String zip = storeAddress.get("postal_code").toString().substring(1,6);
-//			int storeId = storeLoc.get("location_id").getAsInt();
-//			double storeDist = storeLoc.get("distance").getAsDouble();
-//
-//           System.out.println(zip + "***");
-////
-////            stores.add(new Store(storeLoc.get("location_id").toString(),
-////                    storeLoc.get("distance").toString(),
-////                    storeAddress.get("postal_code").toString().substring(1,6),
-////                    storeAddress.get("address_line1").toString()));
-////
-//			System.exit(1);
 		};
 	}
 
