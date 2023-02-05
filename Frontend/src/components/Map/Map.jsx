b42c5777e966721ceb6bbb7d5aa0d73bfd58b2c6
Luis-Medina12 <luis.medina0312@gmail.com>
2023-02-04 23:36:32 -0700

    Updated data fetching to be its own component

--- Frontend/src/components/Map/Map.jsx
+++ Frontend/src/components/Map/Map.jsx
@@ -12,7 +12,7 @@ const Map = ({setCoordinates, setBounds, coordinates, stores, home}) => {
 
     return (
         <div className={classes.mapContainer}>
-            <GoogleMapReact bootstrapURLKeys = {{key: 'AIzaSyCQKgxVlfCM95fX1VzrFc3-JeH7Y4R5Qg4'}}
+            <GoogleMapReact bootstrapURLKeys = {{key: ''}}
                 defaultCenter = {coordinates}
                 center = {coordinates}
                 defaultZoom = {14}
