1fc85eaa60e56be04801669d684678d16ba12021
Luis-Medina12 <69488378+Luis-Medina12@users.noreply.github.com>
2023-02-22 21:29:32 -0700

    Deleting Backend Directory
    Deleting the Beckend Directory and repurposing it as a stand-alone repo.

--- Backend/src/main/java/com/personal/TargetBetterPrice/Store/StoreInterface.java
+++
@@ -1,8 +0,0 @@
-package com.personal.TargetBetterPrice.Store;
-
-import java.util.ArrayList;
-
-public interface StoreInterface {
-    void findStoresSingle(String homeZip, String tcin, String distance);
-    ArrayList<Store> getStores();
-}
