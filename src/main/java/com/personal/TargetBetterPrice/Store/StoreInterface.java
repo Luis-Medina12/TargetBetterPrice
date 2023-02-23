c14ac06437afeba105deb19c363edb892e6cdc3d
Luis-Medina12 <69488378+Luis-Medina12@users.noreply.github.com>
2023-02-22 21:29:54 -0700

    Deleting unneeded directory

--- src/main/java/com/personal/TargetBetterPrice/Store/StoreInterface.java
+++
@@ -1,9 +0,0 @@
-package com.personal.TargetBetterPrice.Store;
-
-import java.util.ArrayList;
-
-public interface StoreInterface {
-    void findStoresSingle(String homeZip, String tcin, String distance);
-    void findStoresMultiple(String homeZip, String tcin, String distance);
-    ArrayList<Store> getStores();
-}
